764ec8b0befde3a2ab66e988bd3f61acfae967f1
Idiakose O. Sunday <idiakosesunday@gmail.com>
2021-01-17 06:54:55 +0100

    Add tests for ProductExceptSelf

--- ProjectFAANG.Tests/ProductExceptSelf_Solution_Tests.cs
+++ ProjectFAANG.Tests/ProductExceptSelf_Solution_Tests.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using Xunit;
+using ProductOfArrayExceptSelf;
+
 namespace ProjectFAANG.Tests
 {
-    public class Solution_Tests
+    public class ProductExceptSelfSolution_Tests
     {
-        public Solution_Tests()
+        [Fact]
+        public void Empty_Array_Returns_Empty_Result()
         {
+            var result = new Solution().ProductExceptSelf(new int[] { });
+            Assert.Empty(result);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 0, 0 }, new int[] { 0, 0 })]
+        [InlineData(new int[] { 1, 2, 3, 4 }, new int [] { 24, 12, 8, 6 })]
+        public void Solution_Works_On_Inputs(int[] input, int[] expected)
+        {
+            var result = new Solution().ProductExceptSelf(input);
+            Assert.Equal(result, expected);
         }
     }
 }
