4c16fabcad04e1b322210549521903411ae02c4f
Idiakose O. Sunday <idiakosesunday@gmail.com>
2021-01-17 06:54:41 +0100

    COpy over code of ProductOfArrayExceptSelf

---
+++ ProductOfArrayExceptSelf/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProductOfArrayExceptSelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = new Solution().ProductExceptSelf(new int[] { 1, 2, 3, 4});
+            result = new Solution().ProductExceptSelf(new int[] { 0, 0 });
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProductOfArrayExceptSelf/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ProductOfArrayExceptSelf
+{
+    public class Solution
+    {
+        public Solution()
+        {
+        }
+
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            int[] solution = new int[nums.Length];
+
+            if (nums.Length == 0)
+                return Array.Empty<int>();
+
+            solution[0] = 1;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                solution[i] = solution[i - 1] * nums[i - 1];
+            }
+            int right = 1;
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                solution[i] *= right;
+                right *= nums[i];
+            }
+
+            return solution;
+        }
+    }
+}
